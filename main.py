65c0381db2e6662dd5f46456c964a70a3a9d448a
Anthony <anthony00029@gmail.com>
2024-02-22 12:39:59 +0100

    added delete_columns function

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ prep = Preprocessing()
 
 train = prep.load_json("data/train.json")
 test = prep.load_json("data/test.json")
+
+prep.delete_columns(train)
+print(train.columns)
--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -2,7 +2,11 @@ import pandas as pd
 import numpy as np
 import json
 
-class Preprocessing():
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_rows", None)
+
+
+class Preprocessing:
     def load_json(self, json_file) -> pd.DataFrame:
         """
         DESCRIPTION
@@ -16,8 +20,21 @@ class Preprocessing():
         """
         with open(json_file) as file:
             dict_json = json.load(file)
-        
-        return pd.DataFrame.from_dict(dict_json)
-        
 
-        
+        return pd.DataFrame.from_dict(dict_json)
+
+    def delete_columns(self, df: pd.DataFrame) -> None:
+        # First, remove ID and URL
+        df.drop(columns=["Url", "PropertyId"], inplace=True)
+
+        # Then, remove columns with 50+ % missing values
+        for column in df.columns:
+            total_values = df[column].size
+            missing_values = df[column].isnull().sum()
+            missing_values_percent = (missing_values / total_values) * 100
+
+            if missing_values_percent > 50:
+                df.drop(columns=column, inplace=True)
+
+        # Finally, delete columns that do not correlate with the price
+        df.drop(columns=["TypeOfProperty", "PostalCode", "TypeOfSale"], inplace=True)
